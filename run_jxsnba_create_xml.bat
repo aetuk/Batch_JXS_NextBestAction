a050852e5846922f9d3cc467c5421ee001157df1
Andrew Etuk <awetuk@gmail.com>
2021-10-18 16:46:00 +0100

    updated
    nbaurl added

--- run_jxsnba_create_xml.bat
+++ run_jxsnba_create_xml.bat
@@ -1,4 +1,5 @@
 set "outputxml=%1.xml" 
+set nbaurl=http://132.145.65.68:8080/bre/postdata
 echo ^<root^> >> %outputxml%
-for /F "tokens=*" %%A in ('jq-win64.exe -c ".[]" %1') do curl -H "Content-Type: application/json" -X POST http://132.145.65.68:8080/bre/postdata -d %%A >> %outputxml%
+for /F "tokens=*" %%A in ('jq-win64.exe -c ".[]" %1') do curl -H "Content-Type: application/json" -X POST %nbaurl% -d %%A >> %outputxml%
 echo ^<^/root^> >> %outputxml%
