c51786edb8740619e5ff98c58d55b703419b5f8d
Andrew Etuk <awetuk@gmail.com>
2022-02-17 22:01:10 +0000

    updates to batch run file
    updates to the batch readme files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Batch_JXS_NextBestAction
  Batch JXS Next Best Action
+ 
+ # Run Batch NBA
+ 
+ # Run Scenario 1
+ 
+ run_jxsnba batchtest.csv batchoutput.csv
+ 
+ batchtest.csv - input customer data and other insights required to make decisions
+ batchoutput.csv - decisions which have been made
--- run_jxsnba.bat
+++ run_jxsnba.bat
@@ -1,9 +1,31 @@
+@echo off
+
+if [%1]==[] goto usage
+if [%2]==[] goto usage
+@echo Executing run_create_jsonfile_from_csv
+
 set inputcsv=%1
 set outputcsv=%2
 set "outputjson=%inputcsv%.json" 
 set "outputjson2=%inputcsv%.json.xml" 
 call run_create_jsonfile_from_csv.bat %inputcsv% %outputjson%
+
+@echo Executing run_jxsnba_create_xml
+
 call run_jxsnba_create_xml.bat %outputjson%  
+
+@echo Executing run_create_csvfile_from_xml
+
 call run_create_csvfile_from_xml.bat %outputjson2% %outputcsv%  
 del %outputjson% 
 del %outputjson2%
+
+
+
+@echo Done.
+goto :eof
+:usage
+@echo Usage: %0 input_csv_file output_csv_file
+exit /B 1
+
+
